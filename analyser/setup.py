b12fca865fee448bfc77b6a6042efc7e0ffc8a5e
Amanda Gaynor <amanda@alertacall.com>
2015-03-05 12:19:25 +0000

    init

---
+++ analyser/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='analyser',
+    version='1.0',
+    py_modules=['AnalyseText', 'text_analyzer'],
+
+    # metadata
+    description='Learning python',
+
+)
